71a6a0466eba2d3c9f1d1ff310945db98d78793e
eraldoakosoft <eraldo450518@outlook.com>
2020-11-01 21:42:46 -0300

    update Navigator

--- src/Navigator.js
+++ src/Navigator.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import Feed from './screens/Feed'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const Tab = createBottomTabNavigator()
 
@@ -9,8 +10,28 @@ export default function MyTabs() {
     return (
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name="Home" component={Feed} />
-                <Tab.Screen name="Settings" component={Feed} />
+                <Tab.Screen
+                    name="Home"
+                    component={Feed}
+                    options={{
+                        tabBarIcon: ({ tintColor }) =>
+                            <Icon name='home' size={30} color={tintColor} />
+                    }}
+                />
+                <Tab.Screen
+                    name="Camera"
+                    component={Feed}
+                    options={{
+                        tabBarIcon: ({ tintColor }) =>
+                            <Icon name='camera' size={30} color={tintColor} />
+                    }} />
+                <Tab.Screen
+                    name="Profile"
+                    component={Feed}
+                    options={{
+                        tabBarIcon: ({ tintColor }) =>
+                            <Icon name='user' size={30} color={tintColor} />
+                    }} />
             </Tab.Navigator>
         </NavigationContainer>
     )
