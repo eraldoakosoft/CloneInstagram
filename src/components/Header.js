e7f12d57835e3a44025f32151d935aa53a718d4c
eraldoakosoft <eraldo450518@outlook.com>
2020-11-12 21:49:10 -0300

    Adicionado o avatar e o nome no Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, Platform, Image } from 'react-native'
+import { connect } from 'react-redux'
+import { Gravatar } from 'react-native-gravatar'
 
 import icon from '../../assets/imgs/icon.png'
 
 class Header extends Component{
     render(){
+        const name = this.props.name || 'Anonymous'
+        const gravatar = this.props.email ? 
+            <Gravatar options={ { email: this.props.email, secure: true } } style={styles.avatar} /> : null
         return(
             <View style={styles.container} >
                 <View style={styles.rowContainer} >
                     <Image source={icon} style={styles.image} />
                     <Text style={styles.title} >Instagram</Text>
+                </View>
+                <View style={styles.userContainer} >
+                    <Text style={styles.user} >{name}</Text>
+                    {gravatar}
                 </View>
             </View>
         )
@@ -21,7 +30,10 @@ const styles = StyleSheet.create({
         marginTop: Platform.OS === 'ios' ? 20 : 0,
         padding: 10,
         borderBottomWidth: 1,
-        borderColor: '#BBB'
+        borderColor: '#BBB',
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     rowContainer: {
         flexDirection: 'row',
@@ -37,7 +49,31 @@ const styles = StyleSheet.create({
         fontFamily: 'shelter',
         height: 30,
         fontSize: 20
+    },
+    userContainer:{
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    user:{
+        fontSize: 10,
+        color: '#888'
+    },
+    avatar:{
+        width: 30,
+        height: 30,
+        marginLeft: 10
     }
 })
 
-export default Header
+const mapStateToProps = ( { user } ) => {
+    return {
+        email: user.email,
+        name: user.name
+    }
+}
+
+//export default Header
+
+
+export default connect(mapStateToProps)(Header)
+
